a1bb7423fbba5e08d46c95c9f3f97ac35d51f744
Mark Siano <marksiano@airbears2-10-142-166-235.airbears2.1918.berkeley.edu>
2017-02-25 19:57:36 -0800

    Initial Commit

---
+++ The Social Network/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  The Social Network
+//
+//  Created by Mark Siano on 2/25/17.
+//  Copyright © 2017 Mark Siano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
